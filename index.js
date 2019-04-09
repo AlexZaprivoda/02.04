7ab1886e4c6486e4aea365daee8ae4a29287d02d
AlexZaprivoda <zaprivoda.alexander@gmail.com>
2019-04-09 15:33:05 +0300

    Added mongoDB

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var express = require('express')
-var cors = require('cors')
+var express = require('express');
+var cors = require('cors');
+const  MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://chat_adm:c2LR4sbeYwD3ysF@cluster0-uvta0.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+var db;
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
-var app = express()
+var app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static("./public"));
@@ -12,27 +16,58 @@ app.use(express.static("./public"));
 let USERS = [];
 app.post("/reg", (req, res) => {
     // console.log(req.body);
-    const userName = req.body.name;
-    if (!userName) {
-        return res.json({
-            response: false
-        });
-    }
+    //----mongo
+    const newUser = {
+        name: req.body.name,
+        email: req.body.email,
+        pass: req.body.pass
+    };
 
-    if (USERS.some((user) => user.name === userName)) {
+    if (db.collection("users").find({ email: req.body.email })){
         res.json({
             response: false
         });
     } else {
-        USERS.push({
-            name: userName,
-            response: null
-        });
-        res.json({
-            response: true
-        });
-        console.log(USERS);
+        db.collection('users').insertOne(newUser, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            res.send(newUser);
+        })
     }
+    
+    console.log(db.collection("users").find({ email: req.body.email }));
+
+    // db.collection('users').insertOne(newUser, (err, result) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.sendStatus(500);
+    //     }
+    //     res.send(newUser);
+    // })
+    //----mongo
+    // const userName = req.body.name;
+    // if (!userName) {
+    //     return res.json({
+    //         response: false
+    //     });
+    // }
+
+    // if (USERS.some((user) => user.name === userName)) {
+    //     res.json({
+    //         response: false
+    //     });
+    // } else {
+    //     USERS.push({
+    //         name: userName,
+    //         response: null
+    //     });
+    //     res.json({
+    //         response: true
+    //     });
+    //     console.log(USERS);
+    // }
 });
 
 app.post("/msg", (req, res) => {
@@ -83,9 +118,18 @@ app.get("/hello", (req, res) => {
     res.send("hello!");
 });
 
-app.listen(PORT, function() {
-    console.log('CORS-enabled web server listening on port 3000')
+client.connect(err => {
+    if (err) {
+        return console.log(err);
+    }
+    db = client.db("chatAPP");
+    // console.log(db);
+    app.listen(PORT, function () {
+        console.log('CORS-enabled web server listening on port 3000')
+    })
 })
+
+
 
 // setInterval(() => {
 //     console.log(USERS.name);
